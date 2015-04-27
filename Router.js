0bdc68bad8af2ae0c9f0ddb59b6769e4bb0db868
Commander-lol <ljcapitanio@gmail.com>
2015-04-27 19:54:43 +0100

    Stop routing after first non-generic route

--- Router.js
+++ Router.js
@@ -123,7 +123,7 @@
                 throw new ReferenceError("Router has no METHOD " + method);
             } else {
                 middleware = middleware.concat(all.filter(matchRoute.bind(matchRoute, path)));
-                middleware = middleware.concat(some.filter(matchRoute.bind(matchRoute, path)));
+                middleware = middleware.push(some.filter(matchRoute.bind(matchRoute, path))[0]);
 
                 for (i = 0, len = middleware.length; i < len; i += 1) {
                     current = middleware[i];
